8009ceed0fb7544cb740735c1797f133056edf46
Ubuntu <ubuntu@ip-172-31-45-126.ec2.internal>
2025-05-19 00:37:37 +0000

    Realização da atividade

--- client.py
+++ client.py
@@ -3,7 +3,10 @@ from constRPYC import * #-
 
 class Client:
   conn = rpyc.connect(SERVER, PORT) # Connect to the server
-  print (conn.root.exposed_value())
-  conn.root.exposed_append(5)       # Call an exposed operation,
-  conn.root.exposed_append(6)       # and append two elements
-  print (conn.root.exposed_value())   # Print the result
+  print("Chaves atuais do dicionário:", conn.root.get_keys())
+  print("Valores atuais do dicionário:", conn.root.get_values())
+  key = input("Digite uma chave para ser adicionada: ")
+  value = input(f"Digite um valor para ser adicionado à chave '{key}': ")
+  conn.root.add_item(key, value)
+  print("Chaves atuais do dicionário:", conn.root.get_keys())
+  print("Valores atuais do dicionário:", conn.root.get_values())
--- server.py
+++ server.py
@@ -2,6 +2,18 @@ import rpyc
 from constRPYC import * #-
 from rpyc.utils.server import ThreadedServer
 
+class RemoteDict(rpyc.Service):
+  dictionary = {}
+
+  def exposed_add_item(self, key, value):
+    self.dictionary[key] = value
+
+  def exposed_get_keys(self):
+    return list(self.dictionary.keys())
+
+  def exposed_get_values(self):
+    return list(self.dictionary.values())
+
 class DBList(rpyc.Service):
   value = []
 
@@ -13,6 +25,6 @@ class DBList(rpyc.Service):
     return self.value
 
 if __name__ == "__main__":
-  server = ThreadedServer(DBList(), port = PORT)
+  server = ThreadedServer(RemoteDict(), port = PORT)
   server.start()
 
